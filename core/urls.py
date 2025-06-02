3200f956a788119c1fd6c6d67a7beb203c901814
mayurwaghela996 <36253435+mayurwaghela996@users.noreply.github.com>
2025-06-02 15:05:35 +0200

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -8,24 +8,50 @@ from employees.views import DashboardView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', DashboardView.as_view(), name='dashboard'),
-    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('dashboard/', DashboardView.as_view(), name='production-dashboard'),
-    # path('', emp_views.DashboardView.as_view(), name='dashboard'),
-    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
     # Employee URLs
     path('employees/', include('employees.urls')),
-    
+
     # Attendance URLs
     path('attendance/', include('attendance.urls')),
-    
+
     # Production URLs
-    # path('production/', include('production.urls')),
     path('production/', include('production.urls')),
+
+    # maintenance URLs
+    path('maintenance/', include('maintenance.urls')),
+
+    # Installation URLs
+    path('installation/', include('installation.urls')),
+
+    # quality URLs
+    path('quality/', include('quality.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # path('', DashboardView.as_view(), name='dashboard'),
+#     path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+#     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+#     # path('dashboard/', DashboardView.as_view(), name='production-dashboard'),
+#     # path('', emp_views.DashboardView.as_view(), name='dashboard'),
+#     # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+#
+#     # Employee URLs
+#     path('employees/', include('employees.urls')),
+#
+#     # Attendance URLs
+#     path('attendance/', include('attendance.urls')),
+#
+#     # Production URLs
+#     # path('production/', include('production.urls')),
+#     path('production/', include('production.urls')),
+# ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
